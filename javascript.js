9b1db6d7030eade6aaac82e9f86597d513806052
bbatal <49416464+bbatal@users.noreply.github.com>
2019-06-07 12:46:38 -0400

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -7,3 +7,33 @@ function Get(yourUrl){
 
 var json_obj = JSON.parse(Get('https://api.exchangeratesapi.io/latest'));
 console.log(JSON.stringify(json_obj.rates));
+
+
+
+function function1() {
+    for (var key in json_obj.rates) {
+      
+    var ul = document.getElementById("List");
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(key, json_obj.rates));
+    li.appendChild(document.createTextNode(" = "));
+    li.appendChild(document.createTextNode(json_obj.rates[key]));
+    ul.appendChild(li);
+  }}
+
+function myfunction() {
+  document.getElementById("myList").innerHTML =  "";
+  for (var key in json_obj.rates) {
+
+  var x = document.getElementById("myNum").value;
+  x *= json_obj.rates[key];
+  var ull = document.getElementById("myList");
+  var lii = document.createElement("Li");
+  lii.appendChild(document.createTextNode("CAD = "));
+  lii.appendChild(document.createTextNode(x));
+  ull.appendChild(lii);
+}};
+
+function1();
+
+
